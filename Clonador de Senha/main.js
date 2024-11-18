e71e2fa8eca1528129d59c850d9287e338a46ed7
guilherme <128057990+Bat-nazi@users.noreply.github.com>
2024-11-18 11:45:11 -0300

    Add files via upload

--- Clonador de Senha/main.js
+++ Clonador de Senha/main.js
@@ -1,5 +1,22 @@
 const numeroSenha = document.querySelector(".parametro-senha-texto");
-let tamanhoSenha = 24;
+let tamanhoSenha = 16;
 numeroSenha.textContent = tamanhoSenha; 
-const botoes = document.querySelector(".parametro-senha-botoes");
-botoes[0].onclik = diminuirTamanho;
+const botaes = document.querySelectorAll(".parametro-senha-botao");
+
+botaes[0].onclick = diminuirTamanho;
+botaes[1].onclick = aumentarTamanho;
+
+function diminuirTamanho(){
+    if(tamanhoSenha>5){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+    }
+}
+
+
+function aumentarTamanho(){
+     if(tamanhoSenha<16){
+     tamanhoSenha = tamanhoSenha+1;
+     numeroSenha.textContent = tamanhoSenha
+    }
+}
