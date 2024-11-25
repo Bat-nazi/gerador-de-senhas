f30f265e3d7831cf00b1d0011d6ae4a9cd425996
guilherme <128057990+Bat-nazi@users.noreply.github.com>
2024-11-25 11:44:32 -0300

    Add files via upload

--- Clonador de Senha/main.js
+++ Clonador de Senha/main.js
@@ -1,22 +1,36 @@
 const numeroSenha = document.querySelector(".parametro-senha-texto");
-let tamanhoSenha = 16;
+let tamanhoSenha = 30;
 numeroSenha.textContent = tamanhoSenha; 
 const botaes = document.querySelectorAll(".parametro-senha-botao");
+const campoSenha = document.querySelector("#campo-senha");
+const letrasMaiusculas = "QWERTYUIOPASDFGHJKLÇZXCVBNM";
+
 
 botaes[0].onclick = diminuirTamanho;
 botaes[1].onclick = aumentarTamanho;
+geraSenha();
 
 function diminuirTamanho(){
-    if(tamanhoSenha>5){
+    if(tamanhoSenha>10){
     tamanhoSenha = tamanhoSenha-1;
     numeroSenha.textContent = tamanhoSenha;
     }
 }
 
-
 function aumentarTamanho(){
-     if(tamanhoSenha<16){
+     if(tamanhoSenha<30){
      tamanhoSenha = tamanhoSenha+1;
      numeroSenha.textContent = tamanhoSenha
     }
+}
+
+
+function geraSenha(){
+    let senha=" ";
+    for(let i= 0; i<tamanhoSenha; i ++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha+letrasMaiusculas[numeroAleatorio];
+    }
+    campoSenha.value=senha;
 }
